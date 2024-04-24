81b22aabeb78bac6afc192c79527390a135abed3
Arthur Opirata <arthurcarneiro098@gmail.com>
2024-04-24 20:13:31 -0300

    correção é atualizaçao

--- app.js
+++ app.js
@@ -1,19 +1,40 @@
-var calculadora = {
+const calculadora = {  
     sumar: function sumarNumeros(n1, n2) {
-       //
+       return n1 + n2
     },
 
     restar: function restarNumeros(n1, n2) {
-       //
+        return  n1 - n2
     },
     
     multiplicar: function multiplicarNumeros(n1, n2) {
-        //
+        return  n1*n2
     },
 
     dividir: function dividirNumeros(n1, n2) {
-        //
+        return  n1/n2
     }
 
 
 };
+
+console.log(calculadora.sumar(3,5))
+
+//forma mais simplificada ao meu ver e tambem um otimo treinamento
+
+const calculadoraPratica = (n1,n2,x)=>{
+    if(x=== "+"){
+        return n1 + n2
+    }else if(x=== "-"){
+        return n1 - n2
+    }else if(x=== "/"){
+        return n1 / n2
+    }else{
+        return n1 * n2
+    }
+    
+
+}
+
+console.log(calculadoraPratica(2,4,'+'))
+
